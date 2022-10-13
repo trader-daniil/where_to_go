8a1a5a71e573be631274fe0843408bdfb5e4371e
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-10-13 13:14:29 +0300

    add image preview admin

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,22 @@
 
 from django.contrib import admin
+import traceback
 
+from django.utils.safestring import mark_safe
 from .models import Place, PlaceImage
 
-class ImageInstance(admin.StackedInline):
+
+class ImageInstance(admin.TabularInline):
     model = PlaceImage
+    
+    readonly_fields = ('show_image',)
+
+    def show_image(self, obj):
+        width = 200 * (obj.image.width/obj.image.height)
+        try:
+            return mark_safe(f'<img src="{obj.image.url}" width="{width}" height=200 />')
+        except Exception:
+            print(traceback.format_exc())
 
 
 @admin.register(Place)
@@ -24,8 +36,13 @@ class AdminPlace(admin.ModelAdmin):
 @admin.register(PlaceImage)
 class AdminPlaceImage(admin.ModelAdmin):
     list_display = ('get_place_image',)
+    """
+    readonly_fields = ('show_image',)
+    
+    def show_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" width="{obj.image.width}" height={obj.image.height} />')
+    """
 
     @admin.display(ordering='-id')
     def get_place_image(self, obj):
         return f'{obj.id} {obj.place.title}'
-
