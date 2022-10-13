5904c7cec03fe4cbf95c20634fa0875e577fcc68
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-10-13 09:40:02 +0300

    add imageinline to admin

--- places/admin.py
+++ places/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 
 from .models import Place, PlaceImage
 
+class ImageInstance(admin.StackedInline):
+    model = PlaceImage
+
 
 @admin.register(Place)
 class AdminPlace(admin.ModelAdmin):
@@ -15,6 +18,8 @@ class AdminPlace(admin.ModelAdmin):
         'title',
         'description_long',
     )
+    inlines = (ImageInstance,)
+
 
 @admin.register(PlaceImage)
 class AdminPlaceImage(admin.ModelAdmin):
