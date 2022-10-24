1f6f8d8f8b86ee8b3ffe520daa8671970a7d07b4
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-10-24 16:28:08 +0300

    split big function on many

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -6,6 +6,33 @@ from places.models import Place, Image
 from django.core.files.base import ContentFile
 
 
+def get_image(image_url):
+    """Создает объект изображения."""
+    response = requests.get(url=image_url)
+    response.raise_for_status()
+    return ContentFile(response.content)
+
+
+def parse_place_characteristics(file_url):
+    """Получение характеристик достопримечательности из ответа."""
+    response = requests.get(url=file_url)
+    response.raise_for_status()
+    return response.json()
+
+
+def create_image(place, image_pos, image):
+    """Связывает сзображение с объектом PlaceImage."""
+    created_image_place, _ = Image.objects.get_or_create(
+        place=place,
+        position=image_pos,
+    )
+    created_image_place.image.save(
+        name=f'{place.title}{image_pos}.jpg',
+        content=image,
+        save=True,
+    )
+
+
 class Command(BaseCommand):
     """Создает объект Place со значением полей из json файла."""
 
@@ -13,32 +40,24 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file_url = options['file_url']
-        response = requests.get(url=file_url)
-        response.raise_for_status()
-        place_characteristics = response.json()
+        place_data = parse_place_characteristics(file_url=file_url)
         created_place, _ = Place.objects.get_or_create(
-            title=place_characteristics['title'],
-            description_short=place_characteristics['description_short'],
-            description_long=place_characteristics['description_long'],
-            lat=float(place_characteristics['coordinates']['lat']),
-            lng=float(place_characteristics['coordinates']['lng']),
+            title=place_data['title'],
+            description_short=place_data['description_short'],
+            description_long=place_data['description_long'],
+            lat=float(place_data['coordinates']['lat']),
+            lng=float(place_data['coordinates']['lng']),
         )
 
         for image_pos, image_url in enumerate(
-            place_characteristics['imgs'],
+            place_data['imgs'],
             start=1,
         ):
-            response = requests.get(url=image_url)
-            response.raise_for_status()
-            image = ContentFile(response.content)
-            created_image_place, _ = Image.objects.get_or_create(
+            image = get_image(image_url=image_url)
+            create_image(
                 place=created_place,
-                position=image_pos,
-            )
-            created_image_place.image.save(
-                name=f'{created_place.title}{image_pos}.jpg',
-                content=image,
-                save=True,
+                image_pos=image_pos,
+                image=image,
             )
 
     def add_arguments(self, parser):
