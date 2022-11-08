29b76c2e39d15c9459a41c7415d5dde4ae0bf8c2
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-11-08 11:33:11 +0300

    add functions in load_place

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -6,16 +6,40 @@ from django.db.utils import IntegrityError
 from places.models import Image, Place
 
 
-def get_image(image_url, image_name):
-    """Создает объект изображения."""
+def create_place(place_data):
+    """Скачивает данные о месте и возвращает его."""
+    created_place, _ = Place.objects.get_or_create(
+        title=place_data['title'],
+        lat=float(place_data['coordinates']['lat']),
+        lng=float(place_data['coordinates']['lng']),
+        defaults={
+            'description_short': place_data.get(
+                'description_short',
+                '',
+            ),
+            'description_long': place_data.get(
+                'description_long',
+                '',
+            ),
+        },
+    )
+    return created_place
+
+
+def create_image(place, image_url, image_pos):
+    """Сохраняет изображение и связывает его в местом."""
+    image_name = f'{place.title}{image_pos}.jpg'
     response = requests.get(url=image_url)
     response.raise_for_status()
-
-    return ContentFile(
+    image = ContentFile(
         content=response.content,
         name=image_name,
     )
-
+    Image.objects.create(
+        place=place,
+        image=image,
+        position=image_pos,
+    )
 
 
 class Command(BaseCommand):
@@ -29,37 +53,15 @@ class Command(BaseCommand):
         response.raise_for_status()
         place_data = response.json()
         try:
-            created_place, _ = Place.objects.get_or_create(
-                title=place_data['title'],
-                lat=float(place_data['coordinates']['lat']),
-                lng=float(place_data['coordinates']['lng']),
-                defaults={
-                    'description_short': place_data.get(
-                        'description_short',
-                        '',
-                    ),
-                    'description_long': place_data.get(
-                        'description_long',
-                        '',
-                    ),
-                },
-            )
+            place = create_place(place_data=place_data)
         except IntegrityError:
             return 'Проверьте заполненность полей title, lat, lng'
 
         for image_pos, image_url in enumerate(place_data['imgs'], start=1):
-            image_name = f'{created_place.title}{image_pos}.jpg'
-
-            response = requests.get(url=image_url)
-            response.raise_for_status()
-            image = ContentFile(
-                content=response.content,
-                name=image_name,
-            )
-            Image.objects.create(
-                place=created_place,
-                image=image,
-                position=image_pos,
+            create_image(
+                place=place,
+                image_url=image_url,
+                image_pos=image_pos,
             )
 
     def add_arguments(self, parser):
