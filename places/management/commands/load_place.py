7cec90dfe33fc1177ea7afdd145d943976e21c01
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-11-14 16:59:34 +0300

    correct returning in load_place

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -11,7 +11,7 @@ def create_place(place_url):
     response = requests.get(url=place_url)
     response.raise_for_status()
     place_data = response.json()
-    created_place, _ = Place.objects.get_or_create(
+    return Place.objects.get_or_create(
         title=place_data['title'],
         lat=place_data['coordinates']['lat'],
         lng=place_data['coordinates']['lng'],
@@ -26,7 +26,6 @@ def create_place(place_url):
             ),
         },
     )
-    return created_place
 
 
 def create_image(place, image_url, image_pos):
@@ -57,19 +56,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file_url = options['file_url']
-        
+
         try:
-            place = create_place(place_url=file_url)
+            place, created = create_place(place_url=file_url)
         except IntegrityError:
             return 'Проверьте заполненность полей title, lat, lng'
-    """
-        for image_pos, image_url in enumerate(place_data['imgs'], start=1):
+        if not created:
+            return 'Место было создано ранее'
+        response = requests.get(url=file_url)
+        response.raise_for_status()
+        image_urls = response.json()['imgs']
+        for image_pos, image_url in enumerate(image_urls, start=1):
             create_image(
                 place=place,
                 image_url=image_url,
                 image_pos=image_pos,
             )
-    """
 
     def add_arguments(self, parser):
         parser.add_argument(
